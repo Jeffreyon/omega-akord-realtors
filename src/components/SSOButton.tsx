92779e8fc7c18c44a47b6dff47207e3b309a2cf4
jeffrey <jeffreyon11@gmail.com>
2023-03-06 12:18:08 +0100

    fix

--- src/components/SSOButton.tsx
+++ src/components/SSOButton.tsx
@@ -12,7 +12,7 @@ let icons = {
     facebook: facebookLogo,
 };
 
-function SSOButton(props: Props) {
+function SSOButton(props: Props | any) {
     let baseStyle =
         "inline-flex items-center justify-center px-8 py-3 text-base font-semibold  transition-all duration-200 text-slate-500 border border-slate-500 hover:bg-slate-200 hover:text-slate-700 w-full flex items-item gap-2";
 
