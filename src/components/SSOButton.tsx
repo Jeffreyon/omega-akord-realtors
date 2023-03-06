0190a8e77d6a4b6b806266909f2577002a9acd92
jeffrey <jeffreyon11@gmail.com>
2023-03-06 12:08:05 +0100

    Fixed type issue

--- src/components/SSOButton.tsx
+++ src/components/SSOButton.tsx
@@ -12,13 +12,20 @@ let icons = {
     facebook: facebookLogo,
 };
 
-function SSOButton(props: Props | any) {
+function SSOButton(props: Props) {
     let baseStyle =
         "inline-flex items-center justify-center px-8 py-3 text-base font-semibold  transition-all duration-200 text-slate-500 border border-slate-500 hover:bg-slate-200 hover:text-slate-700 w-full flex items-item gap-2";
 
+    let icon: string = "";
+
+    if (props.icon) {
+        if (props.icon == "google") icon = icons.google;
+        else if (props.icon == "facebook") icon = icons.facebook;
+    }
+
     return (
         <button {...props} onClick={props.handleClick} className={baseStyle}>
-            {props.icon && <img src={icons[props.icon]} />}
+            {props.icon && <img src={props.icon && icon} />}
             {props.label}
         </button>
     );
